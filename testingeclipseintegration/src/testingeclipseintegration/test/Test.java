c71b561cdfac36ae3721cdc07fc686880ce953ce
Vlad Dracula <vlad@tran.sylvan.ia>
2023-01-31 15:27:59 +0800

    Trying basic OOP

--- testingeclipseintegration/src/testingeclipseintegration/test/Test.java
+++ testingeclipseintegration/src/testingeclipseintegration/test/Test.java
@@ -1,5 +1,10 @@
 package testingeclipseintegration.test;
 
 public class Test {
-
+	public static void main(String[] args) {
+		System.out.println("Diamond City Light");
+		
+		ClassA classA = new ClassA("ABCDEF");
+		System.err.println(classA.getAbc());
+	}
 }
